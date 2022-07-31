c7f49cedea0876644c5fc64aa76885397b9c9dd2
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2022-07-30 21:26:39 -0500

    learning python

---
+++ codewars001.py
@@ -0,0 +1,10 @@
+# Parameters or Edge Cases:
+# Return: the product of the input variables
+# Examples:
+# Psuedo Code: multiply the variables and return the product
+
+# my answer and best practices
+def multiply(a, b):
+    return a * b
+
+# my learning journey with python
