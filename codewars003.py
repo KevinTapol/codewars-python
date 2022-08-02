3f8c68667fbab31ea21c9f85058f516a34e3cf24
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2022-08-02 14:17:37 -0500

    opposit float and/or int

---
+++ codewars003.py
@@ -0,0 +1,14 @@
+# Parameters or Edge Cases:
+# Return: opposite value int or float 
+# Examples: 1: -1
+#           14: -14
+#           -34: 34
+# Psuedo Code: take input float or int multiply by -1 return product
+
+# my answer
+def opposite(number):
+    return number * (-1)
+
+# best practices
+def opposite(number):
+    return -number
