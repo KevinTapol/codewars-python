5b72549bc2e88ceaa9a4b9a63ec372a89f71d04c
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2022-08-03 09:30:43 -0500

    num to string

---
+++ codewars004.py
@@ -0,0 +1,13 @@
+# Parameters or Edge Cases:
+# Return: a number as a string
+# Examples: 123  --> "123"
+#           999  --> "999"
+#           -100 --> "-100"
+# Psuedo Code: return number with quotes
+
+# my answer and best practices
+def number_to_string(num):
+    return str(num)
+
+# most clever
+number_to_string = str
