8c36bd359b53b5e2dbd9a650fcb21c299e376c49
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2022-12-07 10:09:33 -0600

    return the product of two given inputs

--- codewars001.py
+++ codewars001.py
@@ -10,3 +10,48 @@ print(multiply(3,9))  #27
 print(multiply(0,4))  #0
 print(multiply(-2,5))  #-10
 print(multiply(10,9))  #90
+
+# most clever
+# isinstance() function checks whether the object or variable is an instance of the specified class type or data type.
+def multiply(a, b):
+    if isinstance(a, (int, float, complex)):
+        if isinstance(b, (int, float, complex)):
+            return a * b
+
+# using lambda similar to arrow function in javascript
+multiply = lambda x, y: x * y
+
+# using if else 
+def multiply(a, b):
+  if a is not None and b is not None:
+     return a * b
+  else:
+      return None
+
+# using .mul to multiply
+multiply = __import__('operator').mul
+
+# using .mul
+from operator import mul as multiply
+
+# interesting using *= to return the first input as a total
+def multiply(a, b):
+  a *= b
+  return a
+
+# using try except
+def multiply(a, b):
+    try:        
+      return a * b
+    except Exception as e:
+      return e
+    
+# using math. methods
+import math
+def multiply(a, b):
+  return a and b and math.exp(math.log(a) + math.log(b))
+
+# using prod()
+def multiply(*args):
+    from math import prod
+    return prod(args)
