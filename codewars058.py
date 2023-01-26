ac176699d18e07e481cf9bb97cf3dd19d3c58e44
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2023-01-26 11:43:45 -0600

    importing inside a lambda

---
+++ codewars058.py
@@ -0,0 +1,37 @@
+# Get the mean of an array
+# Parameters or Edge Cases:
+    # input will be an array of integers
+    # array will never be empty
+# Return:
+    # the mean or average of the input array
+# Examples:
+    # get_average([2, 2, 2, 2]) # 2
+    # get_average([1, 5, 87, 45, 8, 8]) # 25
+    # get_average([2,5,13,20,16,16,10]) # 11
+    # get_average([1,2,15,15,17,11,12,17,17,14,13,15,6,11,8,7]) # 11
+# Psuedo Code:
+    # return the sum of the array divided by the length of the array
+    # use // to divide and only return the integer do not round up or down
+
+# my answer and best practices and most clever
+def get_average(marks):
+    # return the sum of the array divided by the length of the array
+    # use // to divide and only return the integer do not round up but could round down
+    return sum(marks)//len(marks)
+
+# my answer refactored implicit lambda one liner
+get_average = lambda n: sum(n)//len(n)
+
+print(get_average([2, 2, 2, 2])) # 2
+print(get_average([1, 5, 87, 45, 8, 8])) # 25
+print(get_average([2,5,13,20,16,16,10])) # 11
+print(get_average([1,2,15,15,17,11,12,17,17,14,13,15,6,11,8,7])) # 11
+
+# importing numpy for .mean()
+import numpy
+
+def get_average(marks):
+    return int(numpy.mean(marks))
+
+# same as above but implicit one line function return
+get_average = lambda m: int(__import__("numpy").mean(m))
