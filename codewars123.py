6e67507c34b669825e44c55a4048cadf0994b2b9
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2023-04-26 13:11:21 -0500

    regex create an array with 2 string characters at each index from a string word

--- codewars123.py
+++ codewars123.py
@@ -109,9 +109,3 @@ import re
 
 def solution(s):
     return re.findall('..', s + '_')
-
-from itertools import zip_longest
-
-
-def solution(s):
-    return [''.join(a) for a in zip_longest(s[::2], s[1::2], fillvalue='_')]
