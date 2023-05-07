f8fba6fbbcc3a8d23bcfbb594eec01ac55fbd8cf
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2023-05-06 23:53:19 -0500

    a.center(length) will center string a at the center of string length '{:^{}}'.format ^ is the center align operator

--- codewars133.py
+++ codewars133.py
@@ -101,3 +101,61 @@ print(tower_builder(1)) # ['*']
 print(tower_builder(2)) # [' * ', '***']
 print(tower_builder(3)) # ['  *  ', ' *** ', '*****']
 print(tower_builder(6)) # ['     *     ', '    ***    ', '   *****   ', '  *******  ', ' ********* ', '***********']
+
+# best practices and most clever
+# The center() method will center align the string, using a specified character (space is default) as the fill character.
+# using a for loop starting at 1 and ending at input + 1
+# return an array with each element of "*" multiplied by the current index *2 -1 and center it with the string of length input*2 -1
+def tower_builder(n):
+    return [("*" * (i*2-1)).center(n*2-1) for i in range(1, n+1)]
+
+# if the input is 0 return an empty array
+# loop through and append to an empty array append the current index spaces multiplied by white space concatenated to the current count of "*" and current index white spaces 
+def tower_builder(n_floors):
+    if n_floors == 0:
+        return []
+        
+    count = 1
+    result = []
+
+    for i in range(1, n_floors + 1):
+      stars = '*' * (2 * i - 1)
+      space = ' ' * (n_floors - i)
+      result.append(space + stars + space)
+    
+    return result
+
+# for loop starting at 0 ending at input
+# return an array for each element of the current index maths times white space concatenated by the current index maths "*" concatenated by again the current index maths times white space
+def tower_builder(n):
+    return [" " * (n - i - 1) + "*" * (2*i + 1) + " " * (n - i - 1) for i in range(n)]
+
+# brute force for loop using a count reference instead of index reference
+def tower_builder(n_floors):
+    tower = []
+    spacing = n_floors - 1
+    stars = 1
+    for i in range(0, n_floors):
+        tower.append(' ' * spacing + '*' * stars + spacing * ' ')
+        stars += 2
+        spacing -= 1
+    return tower
+
+# Here ^ is the center align operator. EX '{:^3}'.format('*') evaluates to: ' * '
+def tower_builder(n):
+    length = n * 2 - 1
+    return ['{:^{}}'.format('*' * a, length) for a in range(1, length + 1, 2)]
+
+# declaring the length of each string and using .center() to center the '*'
+def tower_builder(n_floors):
+    width = 2*n_floors - 1
+    return [('*' * (2*i + 1)).center(width) for i in range(n_floors)]
+
+# declaring an empty array and using array methods to append a series of concatenations
+def tower_builder(n_floors):
+    floors = []
+    for i in range(n_floors):
+        n_floors -= 1
+        floors.append(' ' * n_floors + '*' * (i * 2 + 1) + ' ' * n_floors)
+
+    return floors
