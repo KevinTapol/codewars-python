ab98f79d4c5ea7cb1a5f304850e682da01a7d442
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2023-05-25 23:15:28 -0500

    encrypt decrypt string inputs alternating concats of string copy odd/even indexes

--- codewars152.py
+++ codewars152.py
@@ -58,8 +58,25 @@ If the string S is an empty value or the integer N is not positive, return the f
 # Pseudocode:
 """
     # kata 1 encrypt
+    # if the input is empty or null return the input
+    # declare an empty string named result
+    # iterate through the input string
+    # grab all the odd index elements and append them to result
+    # iterate through the input string again
+    # grab all the even index elements and append them to result
+    # if the input integer n is less than 1, return the input
+    # if the input integer n is equal to 1 return result
+    # if the input integer n is more than 1 recall the function but with result as the input string and n -1 as the integer
 
     # kata 2 decrypt
+    # if the input is empty or null return the input
+    # declare a variable as integer and not remainder return of half the input length
+    # create a for loop starting at 0 and ending at the input integer
+    # declare an array copy of the input starting at index 0 and stopping at half
+    # declare an empty string
+    # create a nested for loop starting at 0 and stopping at half + 1 to include half in the iteration
+    # grab the current index copy of b and append it to input then do the same with a alternating concatenation to input of even and odd
+    # outside of the parent for loop return the input
 """
 # my answer for kata 1 encrypt
 def encrypt(input, n):
@@ -70,21 +87,25 @@ def encrypt(input, n):
     result = ""
     # iterate through the input string
     for i,e in enumerate(input):
-        # if the current index is not
+        # grab all the odd index elements and append them to result 
         if i % 2 != 0:
             result += e
+    # iterate through the input string again
     for i,e in enumerate(input):
+        # grab all the even index elements and append them to result
         if i % 2 == 0:
             result += e
+    # if the input integer n is less than 1, return the input
     if n < 1: 
         return input
+    # if the input integer n is equal to 1 return result
     if n == 1:
         return result
+    # if the input integer n is more than 1 recall the function but with result as the input string and n -1 as the integer
     if n > 1:
         return encrypt(result, n -1)
-    return result
 
-# my answer refactored
+# my answer for kata 1 encrypt refactored
 def encrypt(input, n):
     if input in ("", None):
         return input
@@ -101,7 +122,6 @@ def encrypt(input, n):
         return result
     if n > 1:
         return encrypt(result, n -1)
-    return result
 
 print(encrypt("012345", -1)) # "012345"
 print(encrypt("012345", 0)) # "012345"
@@ -116,6 +136,29 @@ print(encrypt("This is a test!", -1)) # "This is a test!"
 print(encrypt("This kata is very interesting!", 1)) # "hskt svr neetn!Ti aai eyitrsig"
 
 # my answer for kata 2 decrypt
+def decrypt(input, n):
+    # if the input is empty or null return the input
+    if input in ("", None):
+        return input
+    # declare a variable as integer and not remainder return of half the input length
+    half = len(input) // 2
+
+    # create a for loop starting at 0 and ending at the input integer
+    for i in range(n):
+        # declare an array copy of the input starting at index 0 and stopping at half
+        a = input[:half]
+        # declare an array copy of the input starting at half and stopping at the last index of input
+        b = input[half:]
+        # declare an empty string
+        input = ""
+        # create a nested for loop starting at 0 and stopping at half + 1 to include half in the iteration
+        for i in range(half + 1):
+            # grab the current index copy of b and append it to input then do the same with a alternating concatenation to input of even and odd
+            input += b[i:i+1] + a[i:i+1]
+    # outside of the parent for loop return the input
+    return input
+
+# my answer for kata 2 decrypt refactored
 def decrypt(input, n):
     if input in ("", None):
         return input
@@ -138,17 +181,60 @@ print(decrypt("This is a test!", 4)) # "This is a test!"
 print(decrypt("This is a test!", -1)) # "This is a test!"
 print(decrypt("hskt svr neetn!Ti aai eyitrsig", 1)) # "This kata is very interesting!"
 
-def decrypt(input, n):
-    if input in ("", None):
-        return input
+# best practices and most clever
+# similar to my code but way cleaner in string copy concatenation
+def decrypt(text, n):
+    if text in ("", None):
+        return text
     
-    ndx = len(input) // 2
+    ndx = len(text) // 2
 
     for i in range(n):
-        a = input[:ndx]
-        b = input[ndx:]
-        input = ""
-        for i in range(ndx + 1):
-            input += b[i:i+1] + a[i:i+1]
-    return input
+        a = text[:ndx]
+        b = text[ndx:]
+        text = "".join(b[i:i+1] + a[i:i+1] for i in range(ndx + 1))
+    return text
 
+
+
+def encrypt(text, n):
+    for i in range(n):
+        text = text[1::2] + text[::2]
+    return text
+
+# similar to best practices and most clever but using if not input for null and empty inputs to return the input
+# also, inside the for loop they are multiline declaring string copies 
+def decrypt(s, n):
+    if not s: return s
+    o, l = len(s) // 2, list(s)
+    for _ in range(n):
+        l[1::2], l[::2] = l[:o], l[o:]
+    return ''.join(l)
+
+
+def encrypt(s, n):
+    if not s: return s
+    for _ in range(n):
+        s = s[1::2] + s[::2]
+    return s
+
+# using text == None for null and empty
+# using the encrypt function inside of decrypt
+def decrypt(text, n):
+    if text == None: return text
+    
+    decodeList = encrypt(list(range(len(text))),n)
+    return ''.join( text[decodeList.index(i)] for i in range(len(text)) )
+
+
+def encrypt(text, n):
+
+    if text == None: return text
+    return encrypt(text[1::2] + text[0::2],n-1) if n>0 else text
+
+# Codewars only one liners for each function
+def encrypt(text, n):
+    return "".join([(text)[((i+1)*2**n-1)%(len(text) + (len(text)+1)%2)] for i in range(len(text))]) if text and not n<=0 else text
+
+def decrypt(text, n):
+    return "".join({num: text[j] for j, num in enumerate([((i+1)*2**n-1)%(len(text) + (len(text)+1)%2) for i in range(len(text))])}.values()) if text and not n<=0 else text
