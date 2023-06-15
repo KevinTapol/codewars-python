2ec26dd18a8774d099eea78ef629765de52d72a7
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2023-06-15 14:27:54 -0500

    in line variable declarations one line enumerate conditionals

--- codewars171.py
+++ codewars171.py
@@ -33,31 +33,90 @@ the first string with each word as lowercase except for the first letter and if 
 """
 # Pseudocode:
 """
-    # create a copy named result of first string lowercase everything then uppercase the first letter of each word with .title()
-    # create a copy named check of the 2nd input string lowercase everything then uppercase the first letter of each word with .title()
-    # if an element of check exists in result and is not the first index of result then lowercase the element of result
-    # return result
+    # declare an empty array result
+    # declare an array of the first string to lowercase split on white space named arr_a
+    # declare an array of the second string to lowercase split on white space named arr_b
+    # enumerate through arr_a to reference both index and element
+    # if the current index is 0 or the current element is not in arr_b append the the element with the first letter of the word to uppercase
+    # if the current index is not 0 or the current element is in arr_b append the current lower case element
+    # convert the array result into a string joining on white space and return it
 """
 # my answer
-def title_case(a,b):
-    # create a copy named result of first string lowercase everything then uppercase the first letter of each word with .title()
-    result = a.title()
-    # create a copy named check of the 2nd input string lowercase everything then uppercase the first letter of each word with .title()
-    
-    # if an element of check exists in result and is not the first index of result then lowercase the element of result
-    # return result
-    # if len(a) == 0:
-    #     return ''
-    if not b:
-        return result
-    else:
-        check = b.title()
-        for e in result:
-            if e in check:
-                e.lower()
-        return result
+def title_case(a, b=''):
+    # declare an empty array result 
+    result = []
+    # declare an array of the first string to lowercase split on white space named arr_a
+    arr_a = a.lower().split()
+    # declare an array of the second string to lowercase split on white space named arr_b
+    arr_b = b.lower().split()
+    # enumerate through arr_a to reference both index and element
+    for i,e in enumerate(arr_a):
+        # if the current index is 0 or the current element is not in arr_b append the the element with the first letter of the word to uppercase
+        if i == 0 or e not in arr_b:
+            result.append(e.capitalize())
+        # if the current index is not 0 or the current element is in arr_b append the current lower case element
+        else:
+            result.append(e)
+    # convert the array result into a string joining on white space and return it
+    return ' '.join(result)
 
-# print(title_case('')) # ''
+# my answer refactored declaring the result array as the one line for loop with conditionals
+def title_case(a, b=''):
+    arr_a = a.lower().split()
+    arr_b = b.lower().split()
+    result = [e.capitalize() if i == 0 or e not in arr_b else e for i,e in enumerate(arr_a)]
+    return ' '.join(result)
+    
+# my answer refactored declaring variables in one line and returning the for loop conditionals array converted to string in one line
+def title_case(a, b=''):
+    arr_a, arr_b = a.lower().split(), b.lower().split()
+    return ' '.join([e.capitalize() if i == 0 or e not in arr_b else e for i,e in enumerate(arr_a)])
+
+print(title_case('')) # ''
+print(title_case('First a of in','an often into')) # 'First A Of In'
 print(title_case('a clash of KINGS', 'a an the of')) # 'A Clash of Kings'
 print(title_case('THE WIND IN THE WILLOWS', 'The In')) # 'The Wind in the Willows'
 print(title_case('the quick brown fox')) # 'The Quick Brown Fox'
+
+# best practices and most clever
+# same as my refactored answer but switching the conditionals
+def title_case(title, minor_words=''):
+    title = title.capitalize().split()
+    minor_words = minor_words.lower().split()
+    return ' '.join([word if word in minor_words else word.capitalize() for word in title])
+
+# using map() and lambdas for arrays
+def title_case(title, minor_words=''):
+    proper_title = ''
+
+    # lowercase the entire list
+    title_words = map(lambda x:x.lower(), title.split(' '))
+    
+    # split the minor word strings
+    dont_cap = minor_words.split(' ')
+    
+    # always upper case the first word
+    title_words = [item.capitalize() for item in title_words]
+    
+    # lower case the split minor words array
+    dont_cap  = [item.lower() for item in dont_cap]
+    
+    #print title_words
+    
+    # loop through the 2nd word to the end word
+    for i in range(1,len(title_words),1):
+        # loop through the minor_words array
+        for item in dont_cap:
+            # if the minor word is equal to the title word, lower case it
+            if item == title_words[i].lower():
+                title_words[i] = title_words[i].lower()
+    
+    return " ".join(title_words)
+
+# one liner for codewars only using enumerate
+def title_case(title, minor_words=''):
+	return ' '.join(w if w in minor_words.lower().split() and i else w.capitalize() for i, w in enumerate(title.lower().split()))
+
+# one liner for codewars only using for element in object instead of enumerate
+def title_case(title, minor_words = ''):
+    return ' '.join(c if c in minor_words.lower().split() else c.title() for c in title.capitalize().split())
