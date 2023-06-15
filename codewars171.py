bf1cc270cdd083466718157b04b3e6a30f3374fa
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2023-06-14 22:53:21 -0500

    description conflicts with test cases

--- codewars171.py
+++ codewars171.py
@@ -17,18 +17,47 @@ title_case('the quick brown fox') # should return: 'The Quick Brown Fox'
 """
 # Parameters or Edge Cases:
 """
-
+inputs will be a string of words of letters that can be upper or lower case
+inputs can be empty
 """
 # Return:
 """
-
+the first string with each word as lowercase except for the first letter and if the 2nd string exists in the input string and is not the first word then lowercase the word and return it
 """
 # Examples:
 """
-
+    ''  => ''
+    'a clash of KINGS', 'a an the of  =>, 'A Clash of Kings'
+    'THE WIND IN THE WILLOWS', 'The In'  => 'The Wind in the Willows'
+    'the quick brown fox'  => 'The Quick Brown Fox'
 """
 # Pseudocode:
 """
-
+    # create a copy named result of first string lowercase everything then uppercase the first letter of each word with .title()
+    # create a copy named check of the 2nd input string lowercase everything then uppercase the first letter of each word with .title()
+    # if an element of check exists in result and is not the first index of result then lowercase the element of result
+    # return result
 """
 # my answer
+def title_case(a,b):
+    # create a copy named result of first string lowercase everything then uppercase the first letter of each word with .title()
+    result = a.title()
+    # create a copy named check of the 2nd input string lowercase everything then uppercase the first letter of each word with .title()
+    
+    # if an element of check exists in result and is not the first index of result then lowercase the element of result
+    # return result
+    # if len(a) == 0:
+    #     return ''
+    if not b:
+        return result
+    else:
+        check = b.title()
+        for e in result:
+            if e in check:
+                e.lower()
+        return result
+
+# print(title_case('')) # ''
+print(title_case('a clash of KINGS', 'a an the of')) # 'A Clash of Kings'
+print(title_case('THE WIND IN THE WILLOWS', 'The In')) # 'The Wind in the Willows'
+print(title_case('the quick brown fox')) # 'The Quick Brown Fox'
